873525f833b21e753e1fe690c5ef0bb345681266
yejinhaoAdmin <810159612@qq.com>
2020-10-12 08:29:39 +0800

    yjh

--- godteam_end/src/main/java/com/zhaolian/demo/data/dao/UsersMapper.java
+++ godteam_end/src/main/java/com/zhaolian/demo/data/dao/UsersMapper.java
@@ -6,12 +6,16 @@ import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 
+import com.zhaolian.demo.web.util.SearchUsersDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface UsersMapper {
-
+    //查询记录总数
+    public int getTotalCount(SearchUsersDTO dto);
+    //分页
+    public List<Users> selectByPage(Map<String, Object> param);
     //房屋认证修改额度
     int updateHome(Users record);
     //车辆认证修改额度
