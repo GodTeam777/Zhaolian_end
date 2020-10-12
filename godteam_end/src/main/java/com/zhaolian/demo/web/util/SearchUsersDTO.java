ccf81f268b174ed588761f5d1665dedfad7ea28e
yejinhaoAdmin <810159612@qq.com>
2020-10-12 17:28:39 +0800

    详细信息

---
+++ godteam_end/src/main/java/com/zhaolian/demo/web/util/SearchUsersDTO.java
@@ -0,0 +1,24 @@
+package com.zhaolian.demo.web.util;
+
+public class SearchUsersDTO {
+    private String search_name;//查询帖子名字
+
+    public String getSearch_name() {
+        return search_name;
+    }
+
+    public void setSearch_name(String search_name) {
+        this.search_name = search_name;
+    }
+
+    @Override
+    public String toString() {
+        return "SearchUsersDTO{" +
+                "search_name='" + search_name + '\'' +
+                '}';
+    }
+
+    public SearchUsersDTO() {
+        super();
+    }
+}
